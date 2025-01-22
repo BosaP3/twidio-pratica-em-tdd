7c8f48d351fa953b3a46461f5e1b947de4a124a5
BosaP3 <pedro.dasilva.313@gmail.com>
2025-01-22 14:53:12 -0300

    Acompanhamento do Projeto

--- src/repositories/PostRepository.test.ts
+++ src/repositories/PostRepository.test.ts
@@ -2,11 +2,12 @@ import { Post } from '../entities/Post'
 import getManagerMock from '../__mocks__/getEntityManagerMock'
 import { getMockContentList } from '../__mocks__/mockContentList'
 import { PostRepository } from './PostRepository'
+import { getMockPost } from '../__mocks__/getMockPost'
 
 describe('PostRepository', () => {
   const mockPostList: Post[] = getMockContentList()
 
-  it('should call getAll method and return all players', async () => {
+  it('should call getAll method and return all posts', async () => {
     const managerMock = await getManagerMock({
       findReturn: mockPostList
     })
@@ -18,4 +19,19 @@ describe('PostRepository', () => {
     expect(managerMock.find).toHaveBeenCalled()
     expect(result).toMatchObject(mockPostList)
   })
+
+  it('should call save method and return new post', async () => {
+    const mockPost = getMockPost()
+    const managerMock = await getManagerMock({
+      saveReturn: mockPost
+    })
+
+    const postRepository = new PostRepository(managerMock)
+
+    const result = await postRepository.save(mockPost)
+
+    expect(managerMock.save).toHaveBeenCalled()
+    expect(result).toMatchObject(mockPost)
+  })
+  
 })
